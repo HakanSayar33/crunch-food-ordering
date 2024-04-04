92a1903665fd5c124d2f65d6f1858c33ad793c5a
Hakan Sayar <hakansayar33@gmail.com>
2024-04-04 13:05:37 +0300

    revise maps

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -91,7 +91,7 @@ const Reservation = () => {
         </form>
         <div className="lg:flex-1 w-full">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48348.66924008447!2d-74.24927437205034!3d40.766603131286395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c254b5958982c3%3A0xb6ab3931055a2612!2sEast%20Orange%2C%20New%20Jersey%2C%20Amerika%20Birle%C5%9Fik%20Devletleri!5e0!3m2!1str!2str!4v1661853137161!5m2!1str!2str"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12238.951384726484!2d32.8370786!3d39.9248825!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d34f190a9cea8f%3A0xd3862ea8248d08a0!2sAn%C4%B1tkabir!5e0!3m2!1str!2str!4v1712225005484!5m2!1str!2str"
             allowFullScreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
